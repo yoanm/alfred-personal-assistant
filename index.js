5410bb9bc6423a8f28b22511a75bc9415bdb0093
Yoanm <yoanm@users.noreply.github.com>
2017-01-29 23:45:06 +0100

    improve

--- index.js
+++ index.js
@@ -8,18 +8,61 @@ if (['dev', 'prod'].indexOf(process.env.NODE_ENV)) {
 const logger = require('./lib/logger');
 const server = require('./lib/server');
 const Alfred = require('./lib/Alfred');
+const Client = require('./lib/Client');
+const PluginManager = require('./lib/PluginManager');
 const pkg = require('./package.json');
+const NestedError = require('nested-error-stacks');
 
 const appName = pkg.name;
 
-const alfred = new Alfred([]);
+const alfred = new Alfred(
+    new Client(),
+    new PluginManager([])
+);
+
+const cleanAndExit = (exitCode = 0) => {
+    let canExit = false;
+    const waitAndExit = arg => {
+
+        if (arg instanceof Error) {
+            exitCode = 1;
+        }
+
+        if (canExit === false) {
+            setTimeout(waitAndExit, 1000);
+        } else {
+            process.exit();
+        }
+    };
+
+    const stopAlfred = arg => {
+
+        if (arg instanceof Error) {
+            exitCode = 1;
+        }
+        logger.stopping('Alfred');
+        return alfred.sleep()
+            .then(() => logger.stopped('Alfred'));
+    };
+    const updateCleanState = arg => {
+        if (arg instanceof Error) {
+            exitCode = 1;
+        }
+        canExit = true;
+    };
+    server.stop()
+        .then(stopAlfred, stopAlfred)
+        .then(updateCleanState, updateCleanState)
+};
+
+process.on('SIGINT', cleanAndExit);
 
 logger.starting(appName);
-
-server.start(alfred)
+return alfred.wakeUp()
+    .then(() => server.start())
     .then(() => logger.started(appName))
     .catch(error => {
-        logger.error(`Exit ${appName} after an error => ${error.stack || error}`);
-
-        process.exit(1);
+        const newError = new NestedError(`Exit ${appName} after an error at initialisation`, error);
+        logger.error(error.stack);
+        cleanAndExit(1);
     });
